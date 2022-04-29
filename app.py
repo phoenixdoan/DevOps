9d0ed2e4b2932feabcdc3560fde5c16ae6ad5d40
TeddyShem <teddyshem100@gmail.com>
2022-04-29 21:38:45 +0300

    added tests

--- app.py
+++ app.py
@@ -3,38 +3,41 @@ from flask import render_template, redirect, request, url_for
 
 app = Flask(__name__)
 
-estimate_list= [{"request":"200 200"}]
+
 @app.route('/')
 def index():
     return render_template('index.html', pageTitle="Vertical Tank Maintenance")
 
+
 @app.route('/about')
 def about():
-    return render_template('about.html',pageTitle="About VTM")
+    return render_template('about.html', pageTitle="About VTM")
+
 
 @app.route('/estimate')
 def estimate():
-    return render_template('estimate.html',pageTitle="Tank Painting Estimate", values = estimate_list)
+    return render_template('estimate.html', pageTitle="Tank Painting Estimate", cost_estimate=0, radius=0, height=0)
+
 
 @app.route('/generate_estimate', methods=['GET', 'POST'])
 def generate_estimate():
-    if request.method =='POST':
+    if request.method == 'POST':
         form = request.form
-        Radius = form['Radius']
-        Height = form['Height']
-        print(Radius)
-        print(Height)
-        estimate_dictionary = {"request": Radius + " " + Height}
-        print(estimate_dictionary)
-        estimate_list.append(estimate_dictionary)
-        print(estimate_list)
-        return redirect(url_for('estimate'))
+        radius = int(form['radius'])
+        height = int(form['height'])
+        print(radius)
+        print(height)
+        pi = 3.14
+        tank_top = pi * radius * radius
+        tank_side = 2 * (pi * (radius * height))
+        total_area = tank_top + tank_side
+        total_square_feet = total_area/144
+        total_mat_cost = total_square_feet * 25.00
+        total_labour_cost = total_square_feet * 15.00
+        total_cost_estimate = total_mat_cost + total_labour_cost
+        return render_template('estimate.html', pageTitle="Tank Painting Estimate",
+                               height=height, radius=radius, cost_estimate=round(total_cost_estimate, 2))
     return redirect(url_for('estimate'))
-
-
-
-
-
 
 
 if __name__ == '__main__':
